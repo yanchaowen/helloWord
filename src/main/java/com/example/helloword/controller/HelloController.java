ed869c8b32dc07b4d63017f2a3b4d2430b827672
yanchaowen <1441774416@qq.com>
2023-04-19 17:49:53 +0800

    第一次提交代码

--- src/main/java/com/example/helloword/controller/HelloController.java
+++ src/main/java/com/example/helloword/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World,my first springboot project!111";
+        return "Hello World,my first springboot project!111222333";
     }
 }
